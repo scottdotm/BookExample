9e9b2bda063674a2eecb035d052d630c575ea633
Scott <Scott@192.168.1.128>
2015-09-23 11:47:30 -0500

    Chapter 1 Book Example

---
+++ src/DuckGame/MiniDuckSimulator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DuckGame;
+
+/**
+ *
+ * @author Scott
+ */
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+    }
+}
